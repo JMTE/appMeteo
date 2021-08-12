3c306d1cf12c1f8802bfe5477ded6c63e77d6743
JMTE <JMTEdeveloper@outlook.es>
2021-08-12 17:01:49 +0200

    Creamos una instancia de Axios para dotar de funcionalidad a nuestro enlace a la API de mapbox, para ello creamos tambien un metodo getter con los parametros que vamos a introducir en el enlace y mediante una plantilla literal podemos introducir el lugar en el enlace a la api

--- Modelos/busquedas.js
+++ Modelos/busquedas.js
@@ -11,13 +11,31 @@ class Busquedas{
         //leer db si existe
     }
 
-    async ciudad (lugar=""){
+    get paramsMapbox(){ //Getter
 
+        return {
+
+            'access_token':'pk.eyJ1IjoidmVyc3Rha2VuIiwiYSI6ImNrczkwajdudTBsdmYycXBmaGlzemsyZHkifQ.muwEN-gxanQwxixQgX9pYg',
+                'limit': 5,
+                'language':'es'
+
+
+        }
+    }
+
+    async ciudad (lugar=""){
+        try{
         //peticion http
 
-        try{
-            const respuesta=await axios.get("https://api.mapbox.com/geocoding/v5/mapbox.places/9%20rue%20Alphonse%20Penaud%20Paris,%2075020%20France.json?access_token=pk.eyJ1IjoidmVyc3Rha2VuIiwiYSI6ImNrczkwajdudTBsdmYycXBmaGlzemsyZHkifQ.muwEN-gxanQwxixQgX9pYg&limit=5")
+        const instance=axios.create({
 
+            baseURL:`https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+            params:this.paramsMapbox
+        })
+
+            const respuesta= await instance.get();
+            // const respuesta=await axios.get("https://api.mapbox.com/geocoding/v5/mapbox.places/9%20rue%20Alphonse%20Penaud%20Paris,%2075020%20France.json?access_token=pk.eyJ1IjoidmVyc3Rha2VuIiwiYSI6ImNrczkwajdudTBsdmYycXBmaGlzemsyZHkifQ.muwEN-gxanQwxixQgX9pYg&limit=5")
+            
             console.log(respuesta.data);
 
             return []; //devuelve todas las ciudades que se corresponden con lugar
