5afcdb9efb69cfade80943325865526d3e6b8acd
JMTE <JMTEdeveloper@outlook.es>
2021-08-12 16:44:58 +0200

    Nos hacemos un perfil en mapbox para utilizar su api y rescatar los valores de las ciudades en un json al hacer la peticion http.

--- Modelos/busquedas.js
+++ Modelos/busquedas.js
@@ -15,11 +15,19 @@ class Busquedas{
 
         //peticion http
 
-        const respuesta=await axios.get("https://reqres.in/api/users?page=2")
+        try{
+            const respuesta=await axios.get("https://api.mapbox.com/geocoding/v5/mapbox.places/9%20rue%20Alphonse%20Penaud%20Paris,%2075020%20France.json?access_token=pk.eyJ1IjoidmVyc3Rha2VuIiwiYSI6ImNrczkwajdudTBsdmYycXBmaGlzemsyZHkifQ.muwEN-gxanQwxixQgX9pYg&limit=5")
 
-        console.log(respuesta.data);
+            console.log(respuesta.data);
 
-        return []; //devuelve todas las ciudades que se corresponden con lugar
+            return []; //devuelve todas las ciudades que se corresponden con lugar
+
+        }catch (error){
+
+            return [];
+        }
+
+        
     }
 
 
